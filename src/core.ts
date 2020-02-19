671b177bc145d2df118ecf0957d5250339d1ef41
Yegor G <yegorich_555@mail.ru>
2020-02-19 15:19:24 +0300

    Core. Set React.PureComponent as Core.Component by default

--- src/core.ts
+++ src/core.ts
@@ -4,7 +4,7 @@
 import React from "react";
 
 declare module Core {
-  export class Component<Props, State> extends React.Component<Props, State> {}
+  export class Component<Props, State> extends React.PureComponent<Props, State> {}
   export type Element = React.ReactElement; // ReactChild
   export type Node = React.ReactNode;
   /* eslint-disable @typescript-eslint/no-explicit-any */
