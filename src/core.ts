752165692ea98674b58fb4ac73de63d9b77caa39
Yegor G <yegorich_555@mail.ru>
2020-02-21 17:24:00 +0300

    Core. Remove forEachChildren. Fix 'Core is undefined' for js files

--- src/core.ts
+++ src/core.ts
@@ -3,17 +3,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
 
+// this is just ts-types
+// @ts-ignore
 declare module Core {
   export class Component<Props, State> extends React.PureComponent<Props, State> {}
   export type Element = React.ReactElement; // ReactChild
   export type Node = React.ReactNode;
   /* eslint-disable @typescript-eslint/no-explicit-any */
-  export function renderElement(type: any, props?: object | null, ...children: any[]): any;
+  export function renderElement(type: any, props?: object | null, ...children: any[]): {};
   /* eslint-enable @typescript-eslint/no-explicit-any */
   export type FormEvent = React.FormEvent<HTMLFormElement>;
-  export function forEachChildren<C>(children: C | C[], fn: (child: C, index: number) => void): void;
 }
-Core.forEachChildren = React.Children.forEach;
+
+// this is just js-functionality
+// @ts-ignore
+// eslint-disable-next-line no-redeclare
+const Core = {};
 Core.renderElement = React.createElement;
 Core.Component = React.Component;
 
