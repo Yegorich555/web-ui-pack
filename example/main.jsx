522851c10ed1869c49be365dded147f9e01e8c50
Yegor G <yegorich_555@mail.ru>
2020-02-24 12:23:12 +0300

    Example. Add path-alias to web-ui-pack

--- example/main.jsx
+++ example/main.jsx
@@ -1,7 +1,7 @@
 import "./styles/main.scss";
-import React, { Component } from "react";
+import { Component } from "react";
 import ReactDom from "react-dom";
-import Form from "../lib/forms/form";
+import { Form } from "web-ui-pack";
 
 class AppContainer extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class AppContainer extends Component {
     return (
       <>
         <h1>WebUIPack Examples</h1>
-        <Form title="Form1">
+        <Form title="Form1" className="uiform">
           <div>Inputs will be here</div>
         </Form>
       </>
