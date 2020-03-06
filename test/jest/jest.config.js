28702bddb1f96bc3ff51fd4ff82e6145dc02ef83
Yegor G <yegorich_555@mail.ru>
2020-03-06 16:50:57 +0300

    Test. Improve naming

--- test/jest/jest.config.js
+++ test/jest/jest.config.js
@@ -5,7 +5,7 @@ const rootDir = process.cwd();
 module.exports = {
   verbose: true,
   rootDir,
-  testMatch: [`${__dirname}/**/*.[jt]s?(x)`],
+  testMatch: [`${__dirname}/**/*.test.[jt]s?(x)`],
   testPathIgnorePatterns: ["/node_modules/", `.eslintrc.js$`, `config.js$`],
   coverageDirectory: "coverage", // {root}/coverage
   collectCoverageFrom: ["lib/**/*.{js,jsx}"],
