3515f032056f932ae847297da98f30b6084a1fa0
Yegor G <yegorich_555@mail.ru>
2020-03-19 17:23:19 +0300

    FormControls. Add to index.ts - export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Core from "./core";
 
 export default Core;
 
+export * from "./forms/form";
+export { default as FormControls } from "./forms/formControls";
 export * from "./controls/validation";
 export * from "./controls/baseControl";
 export * from "./controls/textControl";
-export * from "./forms/form";
