03491b5f0eafbd08d5735ec95f75f4e9cd06ae76
Yegor G <yegorich_555@mail.ru>
2020-06-11 15:17:52 +0300

    Helpers. FocusFirst. Fix behavior when item have already focused

--- src/helpers/focusFirst.ts
+++ src/helpers/focusFirst.ts
@@ -6,9 +6,16 @@
 export default function focusFirst(element: HTMLElement | { focus: () => void }): boolean {
   if (element instanceof HTMLElement) {
     const prev = document.activeElement;
-    element.focus();
-    // such checking required because elements can be indirect disabled or hidden: <fieldset disabled><input/></fieldset>
-    if (document.activeElement !== prev) {
+    const tryFocus = (el: HTMLElement): boolean => {
+      if (prev === el) {
+        return true;
+      }
+      el.focus();
+      // such checking required because elements can be indirect disabled or hidden: <fieldset disabled><input/></fieldset>
+      return document.activeElement !== prev;
+    };
+
+    if (tryFocus(element)) {
       return true;
     }
     // const isVisible = (e: HTMLElement): boolean => {
@@ -16,8 +23,7 @@ export default function focusFirst(element: HTMLElement | { focus: () => void })
     // };
     const items = element.querySelectorAll(`button,input,textarea,[href],select,[tabindex],[contentEditable=true]`);
     for (let i = 0, item = items[0] as HTMLElement; i < items.length; item = items[++i] as HTMLElement) {
-      item.focus();
-      if (document.activeElement !== prev) {
+      if (tryFocus(item)) {
         return true;
       }
     }
--- test/jest/helper.focusFirst.test.js
+++ test/jest/helper.focusFirst.test.js
@@ -33,11 +33,24 @@ describe("helper.focusFirst", () => {
     expect(document.activeElement).toBe(el2);
   });
 
-  test("focus object with propery focis", () => {
+  test("focus object with propery focus", () => {
     expect(focusFirst({})).toBe(false);
     const focus = jest.fn();
     expect(focusFirst({ focus })).toBe(true);
     expect(focus).toBeCalledTimes(1);
+  });
+
+  test("focus again on focused", () => {
+    const el = document.createElement("input");
+    const el2 = document.createElement("input");
+    const container = document.createElement("div");
+    container.appendChild(el);
+    container.appendChild(el2);
+    document.body.appendChild(container);
+    expect(focusFirst(container)).toBe(true);
+    expect(document.activeElement).toBe(el);
+    expect(focusFirst(container)).toBe(true);
+    expect(document.activeElement).toBe(el);
   });
 
   // testing child with invisible element see in test/browser/..
