ae1544c3bf83fbbaa4392995aefade138763ad3b
Yegor G <yegorich_555@mail.ru>
2022-01-11 13:05:20 +0300

    Helpers. onEvent. Add `Node` support (was only HTMLElement)

--- src/helpers/onEvent.ts
+++ src/helpers/onEvent.ts
@@ -1,8 +1,8 @@
 export type TypeOnEvent<K extends keyof HTMLElementEventMap> = (
-  el: HTMLElement,
+  el: HTMLElement | Node,
   type: K | string,
   listener: (
-    this: HTMLElement,
+    this: HTMLElement | Node,
     e: HTMLElementEventMap[K] & {
       target: HTMLElement;
       currentTarget: HTMLElement;
@@ -16,10 +16,10 @@ export type TypeOnEvent<K extends keyof HTMLElementEventMap> = (
  * Despite on addEventListener target & currentTarget is always HTMLElement otherwise it's not fired
  */
 export default function onEvent<K extends keyof HTMLElementEventMap>(
-  el: HTMLElement,
+  el: HTMLElement | Node,
   type: K | string,
   listener: (
-    this: HTMLElement,
+    this: HTMLElement | Node,
     e: HTMLElementEventMap[K] & {
       target: HTMLElement;
       currentTarget: HTMLElement;
@@ -28,7 +28,7 @@ export default function onEvent<K extends keyof HTMLElementEventMap>(
   options?: boolean | AddEventListenerOptions
 ) {
   function wrapper(this: HTMLElement, e: HTMLElementEventMap[K]) {
-    if (e.target instanceof HTMLElement) {
+    if (e.target instanceof Node) {
       listener.call(
         el,
         e as HTMLElementEventMap[K] & {
