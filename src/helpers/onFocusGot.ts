7c418b181aaf508ac88e3482f78d60df7a4600ed
Yegor G <yegorich_555@mail.ru>
2022-01-11 12:16:27 +0300

    Helpers. OnFocusGot. Fix memoryLeaking. Deprecate isHandleActive

--- src/helpers/onFocusGot.ts
+++ src/helpers/onFocusGot.ts
@@ -6,12 +6,14 @@ import onFocusLost from "./onFocusLost";
  * Depsite on focusin onFocusGot event isn't fired several times on chidlren inside element
  * @param {HTMLElement} el HTMLElement to apply `.addEventListener`
  * @param {Function} listener Callback invoked on event
+ * @param options OnFocusLostOptions. onFocusGot depends on onFocusLost
+ * @return Callback with `.removeEventListener`. Call it to remove listener
  * @return {Function} Callback with `.removeEventListener` function. Call it to remove listener
  * */
 export default function onFocusGot(
   el: HTMLElement,
   listener: (this: HTMLElement, ev: HTMLElementEventMap["focusin"]) => any,
-  options?: AddEventListenerOptions & { handleActiveElement?: boolean }
+  options?: Parameters<typeof onFocusLost>[2]
 ): () => void {
   let isLost = true;
   let removeLost: () => void;
@@ -21,15 +23,15 @@ export default function onFocusGot(
     }
     const isFocused = (a: Element) => el === a || el.contains(a);
     const isPrevFocused = e.relatedTarget instanceof Element && isFocused(e.relatedTarget);
-    const isHandleActive = options?.handleActiveElement !== false;
-    if (isHandleActive) {
-      // requires to detect focusLost properly (when console opens)
-      removeLost = onFocusLost(el, () => (isLost = true), options);
-    }
+    // requires to detect focusLost properly (when console opens)
+    removeLost = removeLost || onFocusLost(el, () => (isLost = true), options);
 
     if (!isPrevFocused) {
       listener.call(el, e);
-      options?.once && el.removeEventListener("focusin", focusin);
+      if (options?.once) {
+        el.removeEventListener("focusin", focusin);
+        removeLost?.call(el);
+      }
       isLost = false;
     }
   };
