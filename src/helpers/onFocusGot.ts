0850c0d02e19577b8cd7348419cb3108ac796ebd
Yegor G <yegorich_555@mail.ru>
2022-01-19 13:03:41 +0300

    Helpers. OnFocusGot. Prettify code

--- src/helpers/onFocusGot.ts
+++ src/helpers/onFocusGot.ts
@@ -1,17 +1,14 @@
-// todo unit-tests
-
 import onFocusLost from "./onFocusLost";
 
 /** Fires when element or children gotFocus (happens once)
  * Depsite on focusin onFocusGot event isn't fired several times on chidlren inside element
- * @param {HTMLElement} el HTMLElement to apply `.addEventListener`
+ * @param {HTMLElement} element HTMLElement to apply `.addEventListener`
  * @param {Function} listener Callback invoked on event
  * @param options OnFocusLostOptions. onFocusGot depends on onFocusLost
  * @return Callback with `.removeEventListener`. Call it to remove listener
- * @return {Function} Callback with `.removeEventListener` function. Call it to remove listener
  * */
 export default function onFocusGot(
-  el: HTMLElement,
+  element: HTMLElement,
   listener: (this: HTMLElement, ev: HTMLElementEventMap["focusin"]) => any,
   options?: Parameters<typeof onFocusLost>[2]
 ): () => void {
@@ -21,24 +18,24 @@ export default function onFocusGot(
     if (!isLost) {
       return;
     }
-    const isFocused = (a: Element) => el === a || el.contains(a);
+    const isFocused = (a: Element) => element === a || element.contains(a);
     const isPrevFocused = e.relatedTarget instanceof Element && isFocused(e.relatedTarget);
     // requires to detect focusLost properly (when console opens)
-    removeLost = removeLost || onFocusLost(el, () => (isLost = true), options);
+    removeLost = removeLost || onFocusLost(element, () => (isLost = true), options);
 
     if (!isPrevFocused) {
-      listener.call(el, e);
+      listener.call(element, e);
       if (options?.once) {
-        el.removeEventListener("focusin", focusin);
-        removeLost?.call(el);
+        element.removeEventListener("focusin", focusin);
+        removeLost?.call(element);
       }
       isLost = false;
     }
   };
-  el.addEventListener("focusin", focusin, options);
+  element.addEventListener("focusin", focusin, options);
 
   return () => {
-    removeLost?.call(el);
-    el.removeEventListener("focusin", focusin);
+    removeLost?.call(element);
+    element.removeEventListener("focusin", focusin);
   };
 }
