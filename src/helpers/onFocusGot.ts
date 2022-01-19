f538e346b93729c2cc076a4115ca2b8f4d7d2e1d
Yegor G <yegorich_555@mail.ru>
2022-01-19 15:54:47 +0300

    Helpers. OnFocusGot. Improve remove callback

--- src/helpers/onFocusGot.ts
+++ src/helpers/onFocusGot.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import onFocusLost from "./onFocusLost";
 
 /** Fires when element or children gotFocus (happens once)
@@ -13,29 +14,27 @@ export default function onFocusGot(
   options?: Parameters<typeof onFocusLost>[2]
 ): () => void {
   let isLost = true;
-  let removeLost: () => void;
+  let removeLost: undefined | (() => void);
+  const remove = () => {
+    removeLost?.call(element);
+    element.removeEventListener("focusin", focusin);
+  };
+
   const focusin = (e: FocusEvent) => {
     if (!isLost) {
       return;
     }
-    const isFocused = (a: Element) => element === a || element.contains(a);
-    const isPrevFocused = e.relatedTarget instanceof Element && isFocused(e.relatedTarget);
+    const isFocused = (a: Node) => element === a || element.contains(a);
+    const isPrevFocused = e.relatedTarget instanceof Node && isFocused(e.relatedTarget);
     // requires to detect focusLost properly (when console opens)
     removeLost = removeLost || onFocusLost(element, () => (isLost = true), options);
 
     if (!isPrevFocused) {
       listener.call(element, e);
-      if (options?.once) {
-        element.removeEventListener("focusin", focusin);
-        removeLost?.call(element);
-      }
+      options?.once && remove();
       isLost = false;
     }
   };
   element.addEventListener("focusin", focusin, options);
-
-  return () => {
-    removeLost?.call(element);
-    element.removeEventListener("focusin", focusin);
-  };
+  return remove;
 }
