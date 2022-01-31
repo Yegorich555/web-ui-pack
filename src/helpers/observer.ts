bac5a6746dc9d21de542175eaa018afe8c743875
Yegor G <yegorich_555@mail.ru>
2022-01-31 12:50:56 +0300

    Helpers. Observer. Add onChanged listener

--- src/helpers/observer.ts
+++ src/helpers/observer.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prefer-rest-params */
 // discussions here https://stackoverflow.com/questions/5100376/how-to-watch-for-array-changes
 
-// todo recursive for arrays, nested objects
-// todo single call listener for many changes in time: how to provide listener in this case ?
+/** const a = NaN; a !== a; // true */
+// eslint-disable-next-line no-self-compare
+const isBothNaN = (a: any, b: any) => a !== a && b !== b;
 
-const symObserved = Symbol("__observed");
 export namespace Observer {
-  export type Observed<T extends object = object> = T & {
-    readonly [symObserved]: Observed;
+  export type Observed<T extends object = object> = {
+    // todo recursive
+    [K in keyof T]: T[K] extends object ? Observed<T[K]> : T[K];
   };
 
   export type DateEvent<T extends Date = Date> = {
@@ -26,15 +27,28 @@ export namespace Observer {
 
   export type PropEvent<T, K extends keyof T> = T extends Date ? DateEvent<T> : BasicPropEvent<T, K>;
   export type PropCallback<T> = <K extends keyof T>(event: PropEvent<T, K>) => void;
+
+  export type ObjectEvent<T> = {
+    props: Array<keyof T | "valueOf">;
+    target: T;
+  };
+
+  export type Callback<T> = (event: ObjectEvent<T>) => void;
 }
 
 interface IStored {
   propListeners: Array<Observer.PropCallback<any>>;
+  listeners: Array<Observer.Callback<any>>;
 }
 
 type PartialDateEvent = Omit<Observer.PropEvent<Date, "valueOf">, "target">;
 type PartialPropEvent<T extends object> = Omit<Observer.BasicPropEvent<T, keyof T>, "target" | "prop"> & {
   prop: string | symbol;
+};
+
+const symObserved = Symbol("__observed");
+type PrivateObserved<T extends object> = Observer.Observed<T> & {
+  readonly [symObserved]: PrivateObserved<T>;
 };
 
 // required to avoid prototypeExtending
@@ -57,42 +71,80 @@ const dateWatchKeys = new Set([
   "setYear",
 ]);
 const observeSet = new WeakMap<Observer.Observed, IStored>();
+
+/** Helper to detect object changes;
+ * @example
+ * const raw = { date: new Date(), period: 3 };
+ * const obj = observer.make(raw);
+ * observer.onPropChanged(obj, (e) => console.warn(e));
+ * observer.onChanged(obj, (e) => console.warn(e));
+ * obj.period = 5;
+ */
 const observer = {
-  /** Make observable object; @see Proxy */
+  /** Returns @true if object is observed (is applied observer.make()) */
+  isObserved<T extends object>(obj: T): boolean {
+    return !!(obj as PrivateObserved<T>)[symObserved];
+  },
+  /** Make observable object to detect changes; @see Proxy */
   make<T extends object>(obj: T): Observer.Observed<T> {
-    const prevProxy = (obj as Observer.Observed)[symObserved];
+    const prevProxy = (obj as PrivateObserved<T>)[symObserved];
     if (prevProxy) {
-      return prevProxy as Observer.Observed<T>;
+      return prevProxy as PrivateObserved<T>;
     }
 
     const isDate = obj instanceof Date;
 
+    const listeners: IStored["listeners"] = [];
     const propListeners: IStored["propListeners"] = [];
+    const hasListeners = () => listeners.length || propListeners.length;
+    let changedProps: Array<keyof T> = [];
+    let timeoutId: ReturnType<typeof setTimeout> | undefined | number;
+
     const propChanged = <K extends keyof T>(e: PartialDateEvent | PartialPropEvent<T>) => {
       // eslint-disable-next-line no-use-before-define
       (e as Observer.PropEvent<T, K>).target = proxy;
+      // todo wrap in tryCatch
       propListeners.forEach((f) => f(e as Observer.PropEvent<T, K>));
+      if (listeners.length) {
+        changedProps.push(e.prop as keyof T);
+        if (timeoutId) {
+          return;
+        }
+        // empty timeout filters bunch of changes to single event
+        timeoutId = setTimeout(() => {
+          timeoutId = undefined;
+          const ev: Observer.ObjectEvent<Observer.Observed<T>> = {
+            props: changedProps,
+            // eslint-disable-next-line no-use-before-define
+            target: proxy,
+          };
+          listeners.forEach((f) => f(ev));
+          changedProps = [];
+        });
+      }
     };
-    // const isArray = Array.isArray(obj);
+
     const proxyHandler: ProxyHandler<typeof obj> = {
       set(_t, prop, next) {
         const prev = obj[prop as keyof T];
         // @ts-ignore
         const r = Reflect.set(...arguments);
-        if (propListeners.length) {
+        if (hasListeners()) {
           let isChanged = false;
           if (prev == null || next == null) {
             isChanged = prev !== next;
           } else {
+            // todo check when value NaN: in this case
+            // var a = NaN;
+            // a !== a; // true
             isChanged = (prev as any).valueOf() !== (next as any).valueOf();
           }
           isChanged && propChanged({ prev, next, prop });
         }
-        // console.warn("test", arguments);
         return r;
       },
       deleteProperty(t, prop) {
-        if (!isDate) {
+        if (hasListeners() && !isDate) {
           const prev = t[prop as keyof T];
           propChanged({ prev, next: undefined, prop });
         }
@@ -104,72 +156,68 @@ const observer = {
         // @ts-ignore
         // eslint-disable-next-line @typescript-eslint/ban-types
         const v = Reflect.get(...arguments) as Function;
-        if (typeof v === "function") {
+        // wrap if listeners exists
+        if (hasListeners() && typeof v === "function") {
           if (dateWatchKeys.has(prop as string)) {
             return (...args: any[]) => {
               const prev = (obj as Date).valueOf();
               const r = v.apply(obj as Date, args);
               const next = (obj as Date).valueOf();
-              if (prev !== next) {
+              // Date possible to be NaN;
+              if (prev !== next && !isBothNaN(prev, next)) {
                 propChanged({ prev, next, prop: "valueOf" });
               }
               return r;
             };
           }
+          // return default with correct context
           return v.bind(t);
         }
         return v;
       };
     }
-    const proxy = new Proxy(obj, proxyHandler) as Observer.Observed<T>;
-
+    const proxy = new Proxy(obj, proxyHandler) as PrivateObserved<T>;
     Object.defineProperty(proxy, symObserved, { get: () => proxy });
 
-    const stored: IStored = { propListeners };
-    observeSet.set(proxy, stored);
+    observeSet.set(proxy, { propListeners, listeners });
     return proxy;
   },
 
-  onPropChanged<T extends object>(
-    target: Observer.Observed<T>,
+  /** Listen for any props changing on the object; callback is called per each prop-changing */
+  onPropChanged<T extends Observer.Observed<object>>(
+    target: T,
     callback: Observer.PropCallback<T>
     // options: { once?: boolean }
   ) {
     const o = observeSet.get(target);
     if (!o) {
-      throw new Error("Only observer objects expected. Use make() before");
+      throw new Error("Only Observed objects expected. Use make() before");
     }
-    o.propListeners.push(callback);
+    callback && o.propListeners.push(callback);
+  },
+
+  /** Listen for any changing on the object; callback is called single time per bunch of props-changing */
+  onChanged<T extends Observer.Observed<object>>(
+    target: T,
+    callback: Observer.Callback<T>
+    // options: { once?: boolean }
+  ) {
+    const o = observeSet.get(target);
+    if (!o) {
+      throw new Error("Only Observed objects expected. Use make() before");
+    }
+    callback && o.listeners.push(callback);
   },
 };
 
+// const raw = { date: new Date(), period: 3, internal: { val: "str" } };
+// const obj = observer.make(raw);
+// observer.onPropChanged(obj, (e) => console.warn(e));
+// observer.onChanged(obj, (e) => console.warn(e));
+// obj.period = 5;
+
+// const test = obj.internal;
+// console.warn(test);
+
+Object.seal(observer);
 export default observer;
-
-/** Old logic without Proxy */
-// const arrFn: Array<keyof Array<unknown>> = ["pop", "push", "reverse", "shift", "unshift", "splice", "sort"];
-// // impossible to detect options that wasn't assigned previously
-// export function onObjectChanged<T extends Record<any, unknown>>(
-//   target: T,
-//   listener: <K extends keyof T>(event: { prev: T[K]; next: T[K]; propKey: K; target: T }) => void
-//   // options: { once?: boolean; include?: Array<keyof T>; exclude?: Array<keyof T> }
-// ): void {
-//   Object.keys(target).forEach((k: keyof T) => {
-//     let orig = target[k];
-
-//     Object.defineProperty(target, k, {
-//       get: () => orig,
-//       set: <K extends keyof T>(v: T[K]) => {
-//         const prev = orig as T[K];
-//         orig = v;
-
-//         let isChanged = false;
-//         if (prev == null || v == null) {
-//           isChanged = prev !== v;
-//         } else {
-//           isChanged = (prev as Record<string, unknown>).valueOf() !== (v as Record<string, unknown>).valueOf();
-//         }
-//         isChanged && listener({ prev, next: v, propKey: k, target });
-//       },
-//     });
-//   });
-// }
--- test/jest/helper.observer.test.js
+++ test/jest/helper.observer.test.js
@@ -1,12 +1,13 @@
 import observer from "web-ui-pack/helpers/observer";
 
 beforeEach(() => {
+  jest.useFakeTimers();
   jest.clearAllMocks();
+  jest.clearAllTimers();
 });
 
-describe("helper.onObjectChanged", () => {
-  test("simple object with props null,undefined,boolean,number,string,date,function", () => {
-    const prevDateVal = new Date();
+describe("helper.observer", () => {
+  test("simple object with props null,undefined,boolean,number,string,function", () => {
     const prevFunc = () => "hello";
     const obj = observer.make({
       nullVal: null,
@@ -14,7 +15,6 @@ describe("helper.onObjectChanged", () => {
       boolVal: false,
       numberVal: 0,
       stringVal: "",
-      dateVal: new Date(),
       func: prevFunc,
     });
 
@@ -23,67 +23,97 @@ describe("helper.onObjectChanged", () => {
     expect(objAgain).toBe(obj);
 
     const fn = jest.fn();
+    const fn2 = jest.fn();
     observer.onPropChanged(obj, fn);
+    observer.onChanged(obj, fn2);
+    jest.advanceTimersToNextTimer();
     expect(fn).not.toBeCalled();
 
     obj.nullVal = "";
     expect(obj.nullVal).toBe("");
     expect(fn).toBeCalledTimes(1);
     expect(fn).lastCalledWith({ prev: null, next: "", target: obj, prop: "nullVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["nullVal"], target: obj });
 
     obj.undefVal = null;
     expect(obj.undefVal).toBe(null);
     expect(fn).toBeCalledTimes(2);
     expect(fn).lastCalledWith({ prev: undefined, next: null, target: obj, prop: "undefVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(2);
+    expect(fn2).lastCalledWith({ props: ["undefVal"], target: obj });
 
     obj.boolVal = true;
     expect(obj.boolVal).toBe(true);
     expect(fn).toBeCalledTimes(3);
     expect(fn).lastCalledWith({ prev: false, next: true, target: obj, prop: "boolVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(3);
+    expect(fn2).lastCalledWith({ props: ["boolVal"], target: obj });
 
     obj.numberVal = 123;
     expect(obj.numberVal).toBe(123);
     expect(fn).toBeCalledTimes(4);
     expect(fn).lastCalledWith({ prev: 0, next: 123, target: obj, prop: "numberVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(4);
+    expect(fn2).lastCalledWith({ props: ["numberVal"], target: obj });
 
     obj.stringVal = "str";
     expect(obj.stringVal).toBe("str");
     expect(fn).toBeCalledTimes(5);
     expect(fn).lastCalledWith({ prev: "", next: "str", target: obj, prop: "stringVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(5);
+    expect(fn2).lastCalledWith({ props: ["stringVal"], target: obj });
 
-    const dtVal = Date.now();
-    const dt = new Date(dtVal);
-    obj.dateVal = new Date(dt);
-    expect(obj.dateVal.valueOf()).toBe(dt.valueOf());
-    expect(fn).toBeCalledTimes(6);
-    expect(fn).lastCalledWith({ prev: prevDateVal, next: dt, target: obj, prop: "dateVal" });
+    obj.numberVal = 123;
+    jest.clearAllMocks();
+    expect(obj.numberVal).toBe(123);
+    expect(fn).not.toBeCalled(); // no changes - no events
+    jest.advanceTimersToNextTimer();
+    expect(fn2).not.toBeCalled();
+
+    obj.stringVal = "str";
+    expect(obj.stringVal).toBe("str");
+    expect(fn).not.toBeCalled(); // no changes - no events
+    jest.advanceTimersToNextTimer();
+    expect(fn2).not.toBeCalled();
+
+    obj.boolVal = true;
+    expect(obj.boolVal).toBe(true);
+    expect(fn).not.toBeCalled(); // no changes - no events
+    jest.advanceTimersToNextTimer();
+    expect(fn2).not.toBeCalled();
+
+    obj.boolVal = false;
+    expect(fn).toBeCalledTimes(1); // check if it's called again
+    expect(fn).lastCalledWith({ prev: true, next: false, target: obj, prop: "boolVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["boolVal"], target: obj });
 
     expect(obj.func()).toBe("hello");
     const nextFunc = () => "good";
     obj.func = nextFunc;
     expect(obj.func()).toBe("good");
-    expect(fn).toBeCalledTimes(7);
+    expect(fn).toBeCalledTimes(2);
     expect(fn).lastCalledWith({ prev: prevFunc, next: nextFunc, target: obj, prop: "func" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(2);
+    expect(fn2).lastCalledWith({ props: ["func"], target: obj });
 
-    fn.mockClear();
-    const dt2 = new Date(dtVal);
-    obj.dateVal = dt2;
-    expect(obj.dateVal).toBe(dt2);
-    expect(fn).not.toBeCalled(); // date is changed by dt.valueOf is the same
-
-    obj.numberVal = 123;
-    expect(obj.numberVal).toBe(123);
-    expect(fn).not.toBeCalled(); // no changes - no events
-    obj.stringVal = "str";
-    expect(obj.stringVal).toBe("str");
-    expect(fn).not.toBeCalled(); // no changes - no events
-    obj.boolVal = true;
-    expect(obj.boolVal).toBe(true);
-    expect(fn).not.toBeCalled(); // no changes - no events
-
-    obj.boolVal = false;
-    expect(fn).toBeCalledTimes(1); // check if it's called again
-    expect(fn).lastCalledWith({ prev: true, next: false, target: obj, prop: "boolVal" });
+    // checking if onChanged happend once
+    jest.clearAllMocks();
+    obj.boolVal = "strBoolVal";
+    obj.stringVal = 5;
+    expect(fn).toBeCalledTimes(2);
+    expect(fn2).not.toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["boolVal", "stringVal"], target: obj });
   });
 
   test("assign/delete property", () => {
@@ -117,115 +147,227 @@ describe("helper.onObjectChanged", () => {
   test("for array", () => {
     const arr = observer.make([1, 2, 3]);
     const fn = jest.fn();
+    const fn2 = jest.fn();
     observer.onPropChanged(arr, fn);
+    observer.onChanged(arr, fn2);
 
     // access via index
     arr[0] = "v";
     expect(fn).toBeCalledTimes(1);
     expect(fn).lastCalledWith({ prev: 1, next: "v", target: arr, prop: "0" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["0"], target: arr });
 
     // add new via index
-    fn.mockClear();
+    jest.clearAllMocks();
     const arrLast = arr.length;
     arr[arrLast] = 999;
     expect(fn).toBeCalledTimes(1);
     expect(fn).toBeCalledWith({ prev: undefined, next: 999, target: arr, prop: `${arrLast}` });
     // in this case arr length doesn't fire changes
     // expect(fn).lastCalledWith({ prev: 3, next: 4, target: arr, prop: "length" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: [`${arrLast}`], target: arr });
 
     // push single
-    fn.mockClear();
+    jest.clearAllMocks();
     arr.push(12);
     expect(fn).toBeCalledTimes(1);
     expect(fn).toBeCalledWith({ prev: undefined, next: 12, target: arr, prop: "4" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["4"], target: arr });
 
     // push several at once
     arr.length = 0;
-    fn.mockClear();
+    jest.advanceTimersToNextTimer();
+    jest.clearAllMocks();
     arr.push(55, 66);
     expect(fn).toBeCalledTimes(2);
     expect(fn).toBeCalledWith({ prev: undefined, next: 55, target: arr, prop: "0" });
     expect(fn).toBeCalledWith({ prev: undefined, next: 66, target: arr, prop: "1" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["0", "1"], target: arr });
 
     arr.length = 0;
     arr.push(1, 2, 3);
-    fn.mockClear();
+    jest.clearAllMocks();
     // removing via splice
     arr.splice(0, 2);
     expect(fn).toBeCalled(); // splice produces delete, changing each index and changing length
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // adding via splice
-    fn.mockClear();
+    jest.clearAllMocks();
     arr.splice(2, 0, 88);
     expect(fn).toBeCalledTimes(1); // splice produces append if nothing removed previously
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // replace via splice
-    fn.mockClear();
+    jest.clearAllMocks();
     arr.splice(2, 1, 89);
     expect(fn).toBeCalled(); // splice produces delete, changing each index and changing length
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     arr.length = 0;
     arr.push(1, 2, 3);
-    fn.mockClear();
+    jest.advanceTimersToNextTimer();
+    jest.clearAllMocks();
     // removing last via pop
     arr.pop();
     expect(fn).toBeCalledTimes(2);
     expect(fn).toBeCalledWith({ prev: 3, next: undefined, target: arr, prop: "2" });
     expect(fn).toBeCalledWith({ prev: 3, next: 2, target: arr, prop: "length" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["2", "length"], target: arr });
 
     arr.push(99, 100);
-    fn.mockClear();
+    jest.advanceTimersToNextTimer();
+    jest.clearAllMocks();
     // removing first via shift
     arr.shift(); // shift produces delete, changing each index and changing length
     expect(fn).toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // add several via unshift (at first position)
     arr.length = 0;
     arr.push(1, 2, 3);
-    fn.mockClear();
+    jest.advanceTimersToNextTimer();
+    jest.clearAllMocks();
     arr.unshift(98, 99); // unshift produces changing each index and set at first
     expect(fn).toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // changing via sort
-    fn.mockClear();
+    jest.clearAllMocks();
     arr.sort(); // sort produces changing indexes so event-per-change index
     expect(fn).toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // changing via reverse
-    fn.mockClear();
+    jest.clearAllMocks();
     arr.reverse(); // reverse produces changing indexes so event-per-change index
     expect(fn).toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
 
     // clearing with length: 0
     arr.length = 0;
     arr.push(1, 2, 3);
-    fn.mockClear();
+    jest.advanceTimersToNextTimer();
+    jest.clearAllMocks();
     arr.length = 1;
     expect(fn).toBeCalledTimes(1);
     expect(fn).lastCalledWith({ prev: 3, next: 1, target: arr, prop: "length" });
+    jest.advanceTimersToNextTimer();
+    expect(fn).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["length"], target: arr });
+
     arr.length = 0;
     expect(fn).lastCalledWith({ prev: 1, next: 0, target: arr, prop: "length" });
+    jest.advanceTimersToNextTimer();
+    expect(fn).toBeCalledTimes(2);
+    expect(fn2).lastCalledWith({ props: ["length"], target: arr });
   });
 
-  test("for date", () => {
-    const dt = observer.make(new Date());
+  test("for date (valueof)", () => {
+    let obj = observer.make(new Date());
     const fn = jest.fn();
-    observer.onPropChanged(dt, fn);
+    const fn2 = jest.fn();
+    observer.onPropChanged(obj, fn);
 
-    let prev = dt.valueOf();
-    let next = dt.setHours(0, 0, 0, 0);
+    let prev = obj.valueOf();
+    let next = obj.setHours(0, 0, 0, 0);
     expect(fn).toBeCalledTimes(1);
-    expect(fn).toBeCalledWith({ prev, next, target: dt, prop: "valueOf" });
+    expect(fn).toBeCalledWith({ prev, next, target: obj, prop: "valueOf" });
 
     fn.mockClear();
-    prev = dt.valueOf();
-    next = dt.setDate(12);
+    prev = obj.valueOf();
+    next = obj.setDate(12);
     expect(fn).toBeCalledTimes(1);
-    expect(fn).toBeCalledWith({ prev, next, target: dt, prop: "valueOf" });
+    expect(fn).toBeCalledWith({ prev, next, target: obj, prop: "valueOf" });
 
-    expect(() => dt.getTime()).not.toThrow();
-    expect(() => dt.getFullYear()).not.toThrow();
+    expect(() => obj.getTime()).not.toThrow();
+    expect(() => obj.getFullYear()).not.toThrow();
+
+    // date as part of object
+    jest.clearAllMocks();
+    const prevDateVal = new Date(Date.now() - 1);
+    obj = observer.make({
+      dateVal: prevDateVal,
+    });
+    observer.onPropChanged(obj, fn);
+    observer.onChanged(obj, fn2);
+    const dtVal = Date.now();
+    const dt = new Date(dtVal);
+
+    // date (valueof and ref) is changed
+    obj.dateVal = new Date(dtVal);
+    expect(obj.dateVal.valueOf()).toBe(dtVal);
+    expect(fn).toBeCalledTimes(1);
+    expect(fn).lastCalledWith({ prev: prevDateVal, next: dt, target: obj, prop: "dateVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).lastCalledWith({ props: ["dateVal"], target: obj });
+    // date as object is changed but valueOf isn't changed
+    jest.clearAllMocks();
+    obj.dateVal = new Date(dtVal);
+    expect(fn).not.toBeCalled();
+    jest.advanceTimersToNextTimer();
+    expect(fn2).not.toBeCalled();
   });
 
-  // todo check Object.assign
+  test("Object.assign", () => {
+    const obj = observer.make({ v: 1 });
+    const fn = jest.fn();
+    const fn2 = jest.fn();
+    observer.onPropChanged(obj, fn);
+    observer.onChanged(obj, fn2);
+
+    Object.assign(obj, { v: 2 });
+    expect(fn).toBeCalledTimes(1);
+    expect(fn).toBeCalledWith({ prev: 1, next: 2, target: obj, prop: "v" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).toBeCalledWith({ props: ["v"], target: obj });
+
+    jest.clearAllMocks();
+    Object.assign(obj, { v: 3, nVal: "str" });
+    expect(fn).toBeCalledTimes(2);
+    expect(fn).toBeCalledWith({ prev: 2, next: 3, target: obj, prop: "v" });
+    expect(fn).toBeCalledWith({ prev: undefined, next: "str", target: obj, prop: "nVal" });
+    jest.advanceTimersToNextTimer();
+    expect(fn2).toBeCalledTimes(1);
+    expect(fn2).toBeCalledWith({ props: ["v", "nVal"], target: obj });
+  });
+
+  test("method isObserver(), exceptions etc.", () => {
+    expect(observer.isObserved({ v: 1 })).toBeFalsy(); // on notObserved object
+    const obj = observer.make({ v: 1 });
+    expect(observer.isObserved(obj)).toBeTruthy();
+
+    // on empty callback
+    observer.onPropChanged(obj);
+    expect(() => (obj.v = 2)).not.toThrow();
+    // on empty callback
+    observer.onChanged(obj);
+    jest.advanceTimersToNextTimer(); // onChanged has timeout
+    expect(() => (obj.v = 3)).not.toThrow();
+
+    expect(() => observer.onPropChanged({ v: 1 })).toThrow(); // throw because notObserved
+    expect(() => observer.onChanged({ v: 1 })).toThrow(); // throw because notObserved
+  });
+
+  // todo check collission with changing props on several observed
+  // todo check observer on Map/Set
 });
