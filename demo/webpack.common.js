cadd08574bb9afe4868e6872ec389649017a4e91
Yegor G <yegorich_555@mail.ru>
2022-03-29 17:19:40 +0300

    Demo. Build for github pages

--- demo/webpack.common.js
+++ demo/webpack.common.js
@@ -12,7 +12,7 @@ const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 const srcPath = path.resolve(__dirname, "./src/");
-const destPath = path.resolve(__dirname, "./build/"); // ('../Api/wwwroot')
+const destPath = path.resolve(__dirname, "../docs/");
 const assetsPath = path.resolve(__dirname, "./public/");
 const filesThreshold = 8196; // (bytes) threshold for compression, url-loader plugins
 
