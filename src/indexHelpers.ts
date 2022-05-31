dffc390b66c6e739e3090dc4ef634a77afea4490
Yegor G <yegorich_555@mail.ru>
2022-05-31 13:34:06 +0300

    Helpers. Add missed to index.ts

--- src/indexHelpers.ts
+++ src/indexHelpers.ts
@@ -1,6 +1,8 @@
 export { default as findScrollParent } from "./helpers/findScrollParent";
 export { findScrollParentAll } from "./helpers/findScrollParent";
 export { default as focusFirst } from "./helpers/focusFirst";
+export { default as isEqual } from "./helpers/isEqual";
+export { isBothNaN } from "./helpers/isEqual";
 export { default as isIntoView } from "./helpers/isIntoView";
 
 export { default as nestedProperty } from "./helpers/nestedProperty";
