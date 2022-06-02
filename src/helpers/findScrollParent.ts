361620505cdd08c571382663522ed58a37e571cc
Yegor G <yegorich_555@mail.ru>
2022-06-02 15:23:59 +0300

    Helpers. findScrollParent. Fix case when overflow visible (refactor logic)

--- src/helpers/findScrollParent.ts
+++ src/helpers/findScrollParent.ts
@@ -1,6 +1,6 @@
 // Discussion: https://stackoverflow.com/questions/35939886/find-first-scrollable-parent
+// best solution: https://stackoverflow.com/questions/4880381/check-whether-html-element-has-scrollbars
 
-const vars = ["auto", "overlay"];
 /** Find first parent with active scroll X/Y */
 export default function findScrollParent(el: HTMLElement): HTMLElement | null {
   const p = el.parentElement;
@@ -8,28 +8,30 @@ export default function findScrollParent(el: HTMLElement): HTMLElement | null {
     return null;
   }
 
-  if (p.scrollHeight !== p.clientHeight || p.scrollWidth !== p.clientWidth) {
-    const { overflowX, overflowY } = getComputedStyle(p);
-    if (p.scrollHeight !== p.clientHeight && vars.includes(overflowY)) {
-      return p;
-    }
-    if (p.scrollWidth !== p.clientWidth && vars.includes(overflowX)) {
-      return p;
-    }
-    return null;
+  // it means it's changed
+  if (p.scrollTop > 0) {
+    return p;
   }
+  // try to change and check if it's affected
+  const prev = p.scrollTop;
+  p.scrollTop += 10; // 10 to fix case when zoom applied
+  if (prev !== p.scrollTop) {
+    p.scrollTop = prev;
+    return p;
+  }
+
   return findScrollParent(p);
 }
 
 /** Find all parents with active scroll X/Y */
 export function findScrollParentAll(el: HTMLElement): HTMLElement[] | null {
   let l: HTMLElement | null = el;
-  const root = document.body.parentElement;
   const arr = [];
-  while (l && l !== root) {
-    const s = findScrollParent(l);
-    s && arr.push(s);
-    l = l.parentElement;
+  // eslint-disable-next-line no-constant-condition
+  while (1) {
+    l = findScrollParent(l);
+    if (l) arr.push(l);
+    else break;
   }
   return arr.length ? arr : null;
 }
--- test/jest/helper.findScrollParent.test.js
+++ test/jest/helper.findScrollParent.test.js
@@ -1,4 +1,16 @@
-import findScrollParent from "web-ui-pack/helpers/findScrollParent";
+import findScrollParent, { findScrollParentAll } from "web-ui-pack/helpers/findScrollParent";
+
+afterEach(() => {
+  document.body.innerHTML = "";
+  jest.clearAllMocks();
+  jest.restoreAllMocks();
+});
+
+beforeEach(() => {
+  // default scenario when no items places
+  jest.spyOn(document.body, "scrollTop", "get").mockImplementation(() => 0);
+  jest.spyOn(document.body.parentElement, "scrollTop", "get").mockImplementation(() => 0);
+});
 
 describe("helper.findScrollParent", () => {
   test("deault behavior", () => {
@@ -6,17 +18,29 @@ describe("helper.findScrollParent", () => {
     expect(findScrollParent(el)).toBeNull();
     expect(findScrollParent(document.body)).toBeNull();
 
-    jest.spyOn(document.body, "scrollHeight", "get").mockReturnValue(10);
-    jest.spyOn(document.body, "clientHeight", "get").mockReturnValue(8);
-    // check with overflowY
-    const orig = window.getComputedStyle;
-    jest.spyOn(window, "getComputedStyle").mockImplementation((elem) => {
-      const o = orig(elem);
-      if (elem === document.body) {
-        o.overflowY = "auto";
-      }
-      return o;
-    });
+    jest.restoreAllMocks(); // to allow change srollTop for body
     expect(findScrollParent(el) === document.body).toBeTruthy();
+  });
+
+  test("helper.findScrollParentAll", () => {
+    const el = document.body.appendChild(document.createElement("div"));
+    expect(findScrollParentAll(el)).toBeNull();
+    expect(findScrollParentAll(document.body)).toBeNull();
+
+    jest.restoreAllMocks(); // to allow change srollTop for body
+    jest.spyOn(document.body.parentElement, "scrollTop", "get").mockImplementation(() => 0);
+    expect(findScrollParentAll(el)).toEqual([document.body]);
+
+    // move element to nested
+    const main = document.body.appendChild(document.createElement("main"));
+    main.appendChild(el);
+
+    expect(findScrollParentAll(el)).toEqual([main, document.body]); // because scrollTop possible now
+    // check when main without scroll
+    jest.spyOn(main, "scrollTop", "get").mockImplementation(() => 0);
+    expect(findScrollParentAll(el)).toEqual([document.body]);
+
+    jest.restoreAllMocks(); // to allow change srollTop for body
+    expect(findScrollParentAll(el)).toEqual([main, document.body, document.body.parentElement]); // in reality it's impossible but for simlation it's ok
   });
 });
