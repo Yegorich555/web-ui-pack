f5e86da989137cbfaf62f4fef06c9038b3bcdbbd
Yegor G <yegorich_555@mail.ru>
2022-06-08 12:55:05 +0300

    SpinElement. Fix position. Rename option [overflow] to [inline]. Fix JSX types

--- src/spinElement.ts
+++ src/spinElement.ts
@@ -1,14 +1,13 @@
 /* eslint-disable no-use-before-define */
 import WUPBaseElement, { WUP } from "./baseElement";
 import { styleTransform } from "./helpers/styleHelpers";
-import { isIntoView } from "./indexHelpers";
 
 declare global {
   namespace WUPSpin {
     interface Defaults {
-      /** Place above target in the center (`position: relative` not required);
-       * @defaultValue true */
-      overflow: boolean;
+      /** Place inside parent as inline-block or overflow target in the center (`position: relative` not required);
+       * @defaultValue false */
+      inline?: boolean;
       /** Virtual padding of parentElement
        *  [top, right, bottom, left] or [top/bottom, right/left] in px
        * @defaultValue [4,4] */
@@ -22,9 +21,9 @@ declare global {
       overflowTarget?: HTMLElement | null;
     }
     interface JSXProps<T extends WUPSpinElement> extends WUP.JSXProps<T> {
-      /** Place above parent in the center (`position: relative` not required)
-       * @defaultValue true */
-      overflow?: boolean;
+      /** Place inside parent as inline-block or overflow target in the center (`position: relative` not required);
+       * @defaultValue false */
+      inline?: boolean;
     }
   }
 }
@@ -33,17 +32,16 @@ declare global {
 export default class WUPSpinElement extends WUPBaseElement {
   #ctr = this.constructor as typeof WUPSpinElement;
 
-  static observedOptions = new Set<keyof WUPSpin.Options>(["overflow", "overflowTarget", "overflowOffset"]);
+  static observedOptions = new Set<keyof WUPSpin.Options>(["inline", "overflowTarget", "overflowOffset"]);
   static get observedAttributes(): Array<keyof WUPSpin.Options> {
-    return ["overflow"];
+    return ["inline"];
   }
 
   static get $styleRoot(): string {
     return `:root {
-          --spin-1: #009fbc;
-          --spin-1: orange;
+          --spin-1: #ffa500;
           --spin-2: #fff;
-          --spin-anim-time: 1s;
+          --spin-speed: 1s;
           --spin-size: 2em;
           --spin-width: calc(var(--spin-size) / 6);
         }`;
@@ -53,20 +51,19 @@ export default class WUPSpinElement extends WUPBaseElement {
     return `
       :host,
       :host > div {
-        z-index: 90020;
+        z-index: 100;
         display: inline-block;
         width: var(--spin-size);
         height: var(--spin-size);
         box-sizing: border-box;
         border-radius: 50%;
         top:0; left:0;
-        /*top:50%; left:50%; */
         pointer-events: none;
       }
       :host > div {
         border: var(--spin-width) solid var(--spin-1);
-        border-top: var(--spin-width) solid var(--spin-2);
-        animation: WUP-SPIN-1 var(--spin-anim-time) linear infinite;
+        border-top-color: var(--spin-2);
+        animation: WUP-SPIN-1 var(--spin-speed) linear infinite;
         width: 100%; height: 100%;
       }
       @keyframes WUP-SPIN-1 {
@@ -75,7 +72,6 @@ export default class WUPSpinElement extends WUPBaseElement {
   }
 
   static $defaults: WUPSpin.Defaults = {
-    overflow: true,
     overflowReduceByTarget: true,
     overflowOffset: [4, 4],
   };
@@ -84,9 +80,14 @@ export default class WUPSpinElement extends WUPBaseElement {
     ...this.#ctr.$defaults,
   };
 
+  protected override _opts = this.$options;
+
   $refSpin = document.createElement("div");
 
-  protected override _opts = this.$options;
+  /** Force to update position (when $options.oveflow is true) */
+  $refresh() {
+    this.gotChanges([]);
+  }
 
   protected connectedCallback() {
     this.style.display = "none"; // required to prevent unexpected wrong-render (tied with empty timeout)
@@ -105,39 +106,54 @@ export default class WUPSpinElement extends WUPBaseElement {
   protected override gotChanges(propsChanged: Array<keyof WUPSpin.Options> | null) {
     super.gotChanges(propsChanged);
 
-    this._opts.overflow = this.getBoolAttr("overflow", this._opts.overflow);
+    this._opts.inline = this.getBoolAttr("inline", this._opts.inline);
 
     this.style.display = "";
     this.#prevRect = undefined;
     this.#frameId && window.cancelAnimationFrame(this.#frameId);
     this.#frameId = undefined;
 
-    if (this._opts.overflow) {
-      this.style.position = "fixed";
-      // this.style.position = "absolute";
-      // styleTransform(this, "translate", "-50%,-50%");
+    if (!this._opts.inline) {
+      this.style.position = "absolute";
       const goUpdate = () => {
         this.#prevRect = this.#updatePosition();
         // possible if hidden by target-remove
         this.#frameId = window.requestAnimationFrame(goUpdate);
       };
-
       goUpdate();
     } else {
       this.style.transform = "";
     }
   }
 
-  #prevRect?: DOMRect;
+  get target(): HTMLElement {
+    return this._opts.overflowTarget || (this.parentElement as HTMLElement);
+  }
+
+  get hasRelativeParent(): boolean {
+    return !(!this.offsetParent || getComputedStyle(this.offsetParent as HTMLElement).position !== "relative");
+  }
+
+  #prevRect?: Pick<DOMRect, "width" | "height" | "top" | "left">;
   #frameId?: number;
   /** Update position. Call this method in cases when you changed options */
-  #updatePosition = (): DOMRect | undefined => {
-    const trg = this._opts.overflowTarget || (this.parentElement as HTMLElement);
+  #updatePosition = (): Pick<DOMRect, "width" | "height" | "top" | "left"> | undefined => {
+    const trg = this.target;
     // possible when target removed via set innerHTML (in this case remove-hook doesn't work)
     if (!trg.isConnected) {
       return undefined;
     }
-    const r = trg.getBoundingClientRect();
+
+    const r = { width: trg.offsetWidth, height: trg.offsetHeight, left: trg.offsetLeft, top: trg.offsetTop };
+    // when target has position:relative
+    if (this.offsetParent === trg) {
+      r.top = 0;
+      r.left = 0;
+    } else if (!this.hasRelativeParent) {
+      const { top, left } = trg.getBoundingClientRect();
+      r.top = top;
+      r.left = left;
+    }
 
     if (
       this.#prevRect &&
@@ -148,6 +164,7 @@ export default class WUPSpinElement extends WUPBaseElement {
     ) {
       return this.#prevRect;
     }
+
     const offset = {
       top: this._opts.overflowOffset[0],
       right: this._opts.overflowOffset[1],
@@ -167,9 +184,6 @@ export default class WUPSpinElement extends WUPBaseElement {
     styleTransform(this, "translate", `${left}px,${top}px`);
     styleTransform(this, "scale", scale === 1 ? "" : `${scale}`);
 
-    const viewResult = isIntoView(this, { elRect: r });
-    this.style.display = viewResult.partialVisible || viewResult.hidden ? "none" : "";
-
     // todo oveflow-shadow here ???
 
     return r;
@@ -188,7 +202,7 @@ declare global {
   // add element to tsx/jsx intellisense
   namespace JSX {
     interface IntrinsicElements {
-      [tagName]: WUP.JSXProps<WUPSpinElement>;
+      [tagName]: WUPSpin.JSXProps<WUPSpinElement>;
     }
   }
 }
@@ -196,3 +210,7 @@ declare global {
 // setTimeout(() => {
 //   document.querySelector("main")?.scrollTo({ top: 60 });
 // }, 300);
+
+// test case when target has position:relative
+// test case when target.parent has position:relative
+// test case when no parent with position:relative
