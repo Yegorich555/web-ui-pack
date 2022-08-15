b1554e8d4389bd9ce9ca44462c80bda4b8c8d322
Yegor G <yegorich_555@mail.ru>
2022-08-15 14:22:12 +0300

    Form. Fix `pending failed`

--- src/formElement.ts
+++ src/formElement.ts
@@ -382,7 +382,7 @@ export default class WUPFormElement<
       const p1 = this.$onSubmit?.call(this, ev);
       const p2 = this.onsubmit?.call(this, ev2);
 
-      promiseWait(Promise.all([p1, p2, ev.$waitFor]), 300, this.changePending).then(() => {
+      promiseWait(Promise.all([p1, p2, ev.$waitFor]), 300, (v: boolean) => this.changePending(v)).then(() => {
         if (needReset) {
           this.$controls.forEach((v) => (v.$isDirty = false));
           this.$initModel = this.$model;
