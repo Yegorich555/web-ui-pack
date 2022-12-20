a6201ed7c69ce176123b9be1ff3bcd38da9d88de
Yegor G <yegorich_555@mail.ru>
2022-12-20 18:17:40 +0300

    Helper. localeInfo. Add tests & fix date format for ru-RU

--- src/helpers/localeInfo.ts
+++ src/helpers/localeInfo.ts
@@ -34,23 +34,15 @@ export function getDateFormat(locale?: string): { date: string; time: string; da
   const s = new Date(2222, 0, 3, 4, 5, 6).toLocaleString(locale).replace(/AM|am/, "a");
   // const s = "4:05:06 AM, 1/3/2222".replace(/AM|am/, "a");
   let dateTime = "";
-  let startDate: number | null = null;
   let endDate = 0;
-  let startTime: number | null = null;
   let endTime = 0;
   for (let i = 0, inc = 0; i < s.length; ++i) {
     const c = s.charCodeAt(i) - 48;
     let canAdd = true;
     const isDate = c > 0 && c < 4;
     if (isDate) {
-      if (startDate === null) {
-        startDate = i;
-      }
       endDate = i;
     } else if (c > 3 && c < 7) {
-      if (startTime === null) {
-        startTime = i;
-      }
       endTime = i;
     }
     switch (c) {
@@ -91,9 +83,12 @@ export function getDateFormat(locale?: string): { date: string; time: string; da
     }
   }
 
+  const startDate = /[DMY]/.exec(dateTime)!.index;
+  const startTime = /[hms]/.exec(dateTime)!.index;
+
   const r = {
-    date: dateTime.substring(startDate!, endDate + 1),
-    time: dateTime.substring(startTime!, endTime + 1),
+    date: dateTime.substring(startDate, endDate + 1),
+    time: dateTime.substring(startTime, endTime + 1),
     dateTime,
   };
   return r;
@@ -110,10 +105,10 @@ const localeInfo = {
   sep1000: ",",
   /** Date format, example yyyy-MM-dd */
   date: "yyyy-MM-dd",
-  /** Time format, example hh:mm:ss.fff a */
-  time: "hh:mm:ss.fff a",
-  /** Date time format, example yyyy-MM-dd hh:mm:ss.fff a */
-  dateTime: "yyyy-MM-dd",
+  /** Time format, example hh:mm:ss a */
+  time: "hh:mm:ss a",
+  /** Date time format, example yyyy-MM-dd hh:mm:ss a */
+  dateTime: "yyyy-MM-dd hh:mm:ss a",
   /** Re-define all values (call it if localization changed) */
   refresh: (locale?: string) => {
     const cur = new Intl.DateTimeFormat(locale).resolvedOptions();
@@ -128,4 +123,21 @@ const localeInfo = {
 localeInfo.refresh();
 export default localeInfo;
 
-// todo cover with tests
+/* todo months names replace to locale-defined:
+  new Intl.DateTimeFormat(undefined, { month: "short" }).format(new Date()); >>> "Dec"
+  new Intl.DateTimeFormat(undefined, {month: "long"}).format(new Date()); >>> "December"
+
+  new Intl.DateTimeFormat("ru-RU", { month: "short" }).format(new Date()); >>> "дек."
+  new Intl.DateTimeFormat("ru-RU", { month: "long" }).format(new Date()); >>> "декабрь"
+
+  console.warn(new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(new Date())); => 'Tue'
+  console.warn(new Intl.DateTimeFormat("ru-RU", { weekday: "short" }).format(new Date())); => вт
+*/
+
+// localeInfo.refresh("ru-RU");
+// localeInfo.refresh("ar-AE");
+// const s =
+//   "af-ZA am-ET ar-AE ar-BH ar-DZ ar-EG ar-IQ ar-JO ar-KW ar-LB ar-LY ar-MA arn-CL ar-OM ar-QA ar-SA ar-SD ar-SY ar-TN ar-YE as-IN az-az az-Cyrl-AZ az-Latn-AZ ba-RU be-BY bg-BG bn-BD bn-IN bo-CN br-FR bs-Cyrl-BA bs-Latn-BA ca-ES co-FR cs-CZ cy-GB da-DK de-AT de-CH de-DE de-LI de-LU dsb-DE dv-MV el-CY el-GR en-029 en-AU en-BZ en-CA en-cb en-GB en-IE en-IN en-JM en-MT en-MY en-NZ en-PH en-SG en-TT en-US en-ZA en-ZW es-AR es-BO es-CL es-CO es-CR es-DO es-EC es-ES es-GT es-HN es-MX es-NI es-PA es-PE es-PR es-PY es-SV es-US es-UY es-VE et-EE eu-ES fa-IR fi-FI fil-PH fo-FO fr-BE fr-CA fr-CH fr-FR fr-LU fr-MC fy-NL ga-IE gd-GB gd-ie gl-ES gsw-FR gu-IN ha-Latn-NG he-IL hi-IN hr-BA hr-HR hsb-DE hu-HU hy-AM id-ID ig-NG ii-CN in-ID is-IS it-CH it-IT iu-Cans-CA iu-Latn-CA iw-IL ja-JP ka-GE kk-KZ kl-GL km-KH kn-IN kok-IN ko-KR ky-KG lb-LU lo-LA lt-LT lv-LV mi-NZ mk-MK ml-IN mn-MN mn-Mong-CN moh-CA mr-IN ms-BN ms-MY mt-MT nb-NO ne-NP nl-BE nl-NL nn-NO no-no nso-ZA oc-FR or-IN pa-IN pl-PL prs-AF ps-AF pt-BR pt-PT qut-GT quz-BO quz-EC quz-PE rm-CH ro-mo ro-RO ru-mo ru-RU rw-RW sah-RU sa-IN se-FI se-NO se-SE si-LK sk-SK sl-SI sma-NO sma-SE smj-NO smj-SE smn-FI sms-FI sq-AL sr-BA sr-CS sr-Cyrl-BA sr-Cyrl-CS sr-Cyrl-ME sr-Cyrl-RS sr-Latn-BA sr-Latn-CS sr-Latn-ME sr-Latn-RS sr-ME sr-RS sr-sp sv-FI sv-SE sw-KE syr-SY ta-IN te-IN tg-Cyrl-TJ th-TH tk-TM tlh-QS tn-ZA tr-TR tt-RU tzm-Latn-DZ ug-CN uk-UA ur-PK uz-Cyrl-UZ uz-Latn-UZ uz-uz vi-VN wo-SN xh-ZA yo-NG zh-CN zh-HK zh-MO zh-SG zh-TW zu-ZA";
+// s.split(" ").find((l) => {
+//   localeInfo.refresh(l);
+// });
---
+++ test/jest/helper.localeInfo.test.js
@@ -0,0 +1,75 @@
+// import localeInfo from "../../src/helpers/localeInfo";
+import localeInfo from "web-ui-pack/helpers/localeInfo";
+import * as h from "../testHelper";
+
+describe("helper.localeInfo", () => {
+  test("number format", () => {
+    expect((1234.5).toLocaleString("en-US")).toBe("1,234.5");
+    localeInfo.refresh("en-US");
+    expect(localeInfo.sepDecimal).toBe(".");
+    expect(localeInfo.sep1000).toBe(",");
+
+    expect((1234.5).toLocaleString("ru-RU")).toBe("1\u00A0234,5");
+    localeInfo.refresh("ru-RU");
+    expect(localeInfo.sepDecimal).toBe(",");
+    expect(localeInfo.sep1000).toBe("\u00A0"); // 160 code (Non-breaking space; &nbsp;)
+
+    expect((1234.5).toLocaleString("pl-PL")).toBe("1234,5");
+    localeInfo.refresh("pl-PL");
+    expect(localeInfo.sepDecimal).toBe(",");
+    expect(localeInfo.sep1000).toBe(""); // empty
+  });
+
+  test("date & time format", () => {
+    expect(new Date(2222, 0, 3, 4, 5, 6).toLocaleString("en-US")).toBe("1/3/2222, 4:05:06 AM");
+    localeInfo.refresh("en-US");
+    expect(localeInfo.date).toBe("M/D/YYYY");
+    expect(localeInfo.time).toBe("h:mm:ss a");
+    expect(localeInfo.dateTime).toBe("M/D/YYYY, h:mm:ss a");
+
+    expect(new Date(2222, 0, 3, 4, 5, 6).toLocaleString("ru-RU")).toBe("03.01.2222, 04:05:06");
+    localeInfo.refresh("ru-RU");
+    expect(localeInfo.date).toBe("DD.MM.YYYY");
+    expect(localeInfo.time).toBe("hh:mm:ss");
+    expect(localeInfo.dateTime).toBe("DD.MM.YYYY, hh:mm:ss");
+
+    expect(new Date(2222, 0, 3, 4, 5, 6).toLocaleString("pl-PL")).toBe("3.01.2222, 04:05:06");
+    localeInfo.refresh("pl-PL");
+    expect(localeInfo.date).toBe("D.MM.YYYY");
+    expect(localeInfo.time).toBe("hh:mm:ss");
+    expect(localeInfo.dateTime).toBe("D.MM.YYYY, hh:mm:ss");
+  });
+
+  test("all locales", () => {
+    // expect(new Intl.DateTimeFormat("ar-BH").resolvedOptions().numberingSystem).not.toBe("latn");
+    h.mockConsoleWarn();
+    const s =
+      "af-ZA am-ET ar-AE ar-BH ar-DZ ar-EG ar-IQ ar-JO ar-KW ar-LB ar-LY ar-MA arn-CL ar-OM ar-QA ar-SA ar-SD ar-SY ar-TN ar-YE as-IN az-az az-Cyrl-AZ az-Latn-AZ ba-RU be-BY bg-BG bn-BD bn-IN bo-CN br-FR bs-Cyrl-BA bs-Latn-BA ca-ES co-FR cs-CZ cy-GB da-DK de-AT de-CH de-DE de-LI de-LU dsb-DE dv-MV el-CY el-GR en-029 en-AU en-BZ en-CA en-cb en-GB en-IE en-IN en-JM en-MT en-MY en-NZ en-PH en-SG en-TT en-US en-ZA en-ZW es-AR es-BO es-CL es-CO es-CR es-DO es-EC es-ES es-GT es-HN es-MX es-NI es-PA es-PE es-PR es-PY es-SV es-US es-UY es-VE et-EE eu-ES fa-IR fi-FI fil-PH fo-FO fr-BE fr-CA fr-CH fr-FR fr-LU fr-MC fy-NL ga-IE gd-GB gd-ie gl-ES gsw-FR gu-IN ha-Latn-NG he-IL hi-IN hr-BA hr-HR hsb-DE hu-HU hy-AM id-ID ig-NG ii-CN in-ID is-IS it-CH it-IT iu-Cans-CA iu-Latn-CA iw-IL ja-JP ka-GE kk-KZ kl-GL km-KH kn-IN kok-IN ko-KR ky-KG lb-LU lo-LA lt-LT lv-LV mi-NZ mk-MK ml-IN mn-MN mn-Mong-CN moh-CA mr-IN ms-BN ms-MY mt-MT nb-NO ne-NP nl-BE nl-NL nn-NO no-no nso-ZA oc-FR or-IN pa-IN pl-PL prs-AF ps-AF pt-BR pt-PT qut-GT quz-BO quz-EC quz-PE rm-CH ro-mo ro-RO ru-mo ru-RU rw-RW sah-RU sa-IN se-FI se-NO se-SE si-LK sk-SK sl-SI sma-NO sma-SE smj-NO smj-SE smn-FI sms-FI sq-AL sr-BA sr-CS sr-Cyrl-BA sr-Cyrl-CS sr-Cyrl-ME sr-Cyrl-RS sr-Latn-BA sr-Latn-CS sr-Latn-ME sr-Latn-RS sr-ME sr-RS sr-sp sv-FI sv-SE sw-KE syr-SY ta-IN te-IN tg-Cyrl-TJ th-TH tk-TM tlh-QS tn-ZA tr-TR tt-RU tzm-Latn-DZ ug-CN uk-UA ur-PK uz-Cyrl-UZ uz-Latn-UZ uz-uz vi-VN wo-SN xh-ZA yo-NG zh-CN zh-HK zh-MO zh-SG zh-TW zu-ZA";
+    const alls = s.split(" ");
+    for (let i = 0; i < alls.length; ++i) {
+      expect(() => localeInfo.refresh(alls[i])).not.toThrow();
+    }
+    h.unMockConsoleWarn();
+  });
+});
+
+// const alls =
+//   "af-ZA am-ET ar-AE ar-BH ar-DZ ar-EG ar-IQ ar-JO ar-KW ar-LB ar-LY ar-MA arn-CL ar-OM ar-QA ar-SA ar-SD ar-SY ar-TN ar-YE as-IN az-az az-Cyrl-AZ az-Latn-AZ ba-RU be-BY bg-BG bn-BD bn-IN bo-CN br-FR bs-Cyrl-BA bs-Latn-BA ca-ES co-FR cs-CZ cy-GB da-DK de-AT de-CH de-DE de-LI de-LU dsb-DE dv-MV el-CY el-GR en-029 en-AU en-BZ en-CA en-cb en-GB en-IE en-IN en-JM en-MT en-MY en-NZ en-PH en-SG en-TT en-US en-ZA en-ZW es-AR es-BO es-CL es-CO es-CR es-DO es-EC es-ES es-GT es-HN es-MX es-NI es-PA es-PE es-PR es-PY es-SV es-US es-UY es-VE et-EE eu-ES fa-IR fi-FI fil-PH fo-FO fr-BE fr-CA fr-CH fr-FR fr-LU fr-MC fy-NL ga-IE gd-GB gd-ie gl-ES gsw-FR gu-IN ha-Latn-NG he-IL hi-IN hr-BA hr-HR hsb-DE hu-HU hy-AM id-ID ig-NG ii-CN in-ID is-IS it-CH it-IT iu-Cans-CA iu-Latn-CA iw-IL ja-JP ka-GE kk-KZ kl-GL km-KH kn-IN kok-IN ko-KR ky-KG lb-LU lo-LA lt-LT lv-LV mi-NZ mk-MK ml-IN mn-MN mn-Mong-CN moh-CA mr-IN ms-BN ms-MY mt-MT nb-NO ne-NP nl-BE nl-NL nn-NO no-no nso-ZA oc-FR or-IN pa-IN pl-PL prs-AF ps-AF pt-BR pt-PT qut-GT quz-BO quz-EC quz-PE rm-CH ro-mo ro-RO ru-mo ru-RU rw-RW sah-RU sa-IN se-FI se-NO se-SE si-LK sk-SK sl-SI sma-NO sma-SE smj-NO smj-SE smn-FI sms-FI sq-AL sr-BA sr-CS sr-Cyrl-BA sr-Cyrl-CS sr-Cyrl-ME sr-Cyrl-RS sr-Latn-BA sr-Latn-CS sr-Latn-ME sr-Latn-RS sr-ME sr-RS sr-sp sv-FI sv-SE sw-KE syr-SY ta-IN te-IN tg-Cyrl-TJ th-TH tk-TM tlh-QS tn-ZA tr-TR tt-RU tzm-Latn-DZ ug-CN uk-UA ur-PK uz-Cyrl-UZ uz-Latn-UZ uz-uz vi-VN wo-SN xh-ZA yo-NG zh-CN zh-HK zh-MO zh-SG zh-TW zu-ZA";
+
+// alls
+//   .split(" ") //
+//   .map((l) => new Intl.DateTimeFormat(l).resolvedOptions())
+//   .find((l) => {
+//     if (l.numberingSystem !== "latn") {
+//       console.warn(l);
+//       console.log(new Intl.DateTimeFormat(l.locale, {}).format(new Date()));
+//     }
+//     return false;
+//   });
+
+// Intl.NumberFormat("en-US", {
+//   style: "currency",
+//   currency: "USD",
+//   maximumFractionDigits: 0,
+//   minimumFractionDigits: 0,
+// }).format(1234.5); '$1,234.50'
