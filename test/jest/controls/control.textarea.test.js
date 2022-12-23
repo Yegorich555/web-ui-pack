aaef9a3a538d7668a7036c1781e3bacdc9336718
Yegor G <yegorich_555@mail.ru>
2022-12-23 16:08:18 +0300

    Textarea. Tests. Cover 100%

--- test/jest/controls/control.textarea.test.js
+++ test/jest/controls/control.textarea.test.js
@@ -1,15 +1,32 @@
 import { WUPTextareaControl } from "web-ui-pack";
 import { initTestBaseControl } from "./baseControlTest";
 import testTextControl from "./control.textTest";
-// import * as h from "../../testHelper";
+import * as h from "../../testHelper";
 
 /** @type WUPTextareaControl */
-let testEl;
-initTestBaseControl({ type: WUPTextareaControl, htmlTag: "wup-textarea", onInit: (e) => (testEl = e) });
+let el;
+initTestBaseControl({ type: WUPTextareaControl, htmlTag: "wup-textarea", onInit: (e) => (el = e) });
 
 describe("control.textarea", () => {
-  testTextControl(() => testEl, {
+  testTextControl(() => el, {
     validations: {},
     // validationsSkip: [],
+  });
+
+  test("Enter key works properly", async () => {
+    const form = document.body.appendChild(document.createElement("wup-form"));
+    const onSubmit = jest.fn();
+    form.$onSubmit = onSubmit;
+    form.appendChild(el);
+
+    // Enter must add multiline instead of submit
+    el.focus();
+    await h.wait(1);
+    const isPrevented = !el.dispatchEvent(
+      new KeyboardEvent("keydown", { key: "Enter", bubbles: true, cancelable: true })
+    );
+    await h.wait(1);
+    expect(isPrevented).toBe(false);
+    expect(onSubmit).toBeCalledTimes(0);
   });
 });
