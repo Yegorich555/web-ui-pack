4409f8ffe0a4e4d34ef8e6b0475f408e7a78a4e8
Yegor G <yegorich_555@mail.ru>
2023-01-19 11:56:51 +0300

    Helper. scrolled. Fix browser tests

--- test/browser/helper.scrolled.test.jsx
+++ test/browser/helper.scrolled.test.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable jsx-a11y/no-autofocus */
-const scrollCarousel = require("web-ui-pack/helpers/scrollCarousel");
+const WUPScrolled = require("web-ui-pack/helpers/scrolled").default;
 
 beforeAll(async () => {
   await page.emulateMediaFeatures([
@@ -14,19 +13,21 @@ beforeAll(async () => {
       </ul>
     );
     let n = 3;
-    scrollCarousel(document.querySelector("ul"), () => {
-      const arr = [];
-      for (let i = 0; i < 3; ++i) {
-        const li = document.createElement("li");
-        li.textContent = `Item ${++n}`;
-        arr.push(li);
-      }
-      return arr;
+    window._scrolled = new WUPScrolled(document.querySelector("ul"), {
+      onRender: () => {
+        const arr = [];
+        for (let i = 0; i < 3; ++i) {
+          const li = document.createElement("li");
+          li.textContent = `Item ${++n}`;
+          arr.push(li);
+        }
+        return arr;
+      },
     });
   });
 });
 
-describe("helper.scrollCarousel", () => {
+describe("helper.scrolled (carousel)", () => {
   test("ordinary behavior", async () => {
     const el = await page.$("ul");
     const boundingBox = await el.boundingBox();
@@ -41,7 +42,7 @@ describe("helper.scrollCarousel", () => {
       })
     ).toBe(0); // scrolling is not started
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchInlineSnapshot(
-      `"<div id="app"><ul style="max-height: 54px; overflow: hidden; touch-action: none;"><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
+      `"<div id="app"><ul style="overflow: hidden; touch-action: none; max-height: 54px;"><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
     );
 
     await page.waitForTimeout("50");
@@ -50,12 +51,12 @@ describe("helper.scrollCarousel", () => {
     await page.waitForTimeout("1000");
     expect(await page.evaluate(() => document.querySelector("ul").scrollTop)).toBe(0); // scrolling is finished and prev items removed
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchInlineSnapshot(
-      `"<div id="app"><ul style="max-height: 54px; overflow: hidden; touch-action: none;"><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
+      `"<div id="app"><ul style="overflow: hidden; touch-action: none; max-height: 54px;"><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
     );
 
     await page.mouse.wheel({ deltaY: -100 });
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchInlineSnapshot(
-      `"<div id="app"><ul style="max-height: 54px; overflow: hidden; touch-action: none;"><li>Item 7</li><li>Item 8</li><li>Item 9</li><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
+      `"<div id="app"><ul style="overflow: hidden; touch-action: none; max-height: 54px;"><li>Item 7</li><li>Item 8</li><li>Item 9</li><li>Item 4</li><li>Item 5</li><li>Item 6</li></ul></div>"`
     );
     expect(await page.evaluate(() => document.querySelector("ul").scrollTop)).toBe(54); // scrolling is not started but elemens is added at the top
 
@@ -65,7 +66,7 @@ describe("helper.scrollCarousel", () => {
     await page.waitForTimeout("1000");
     expect(await page.evaluate(() => document.querySelector("ul").scrollTop)).toBe(0); // scrolling is finished and prev items removed
     expect(await page.evaluate(() => document.body.innerHTML)).toMatchInlineSnapshot(
-      `"<div id="app"><ul style="max-height: 54px; overflow: hidden; touch-action: none;"><li>Item 7</li><li>Item 8</li><li>Item 9</li></ul></div>"`
+      `"<div id="app"><ul style="overflow: hidden; touch-action: none; max-height: 54px;"><li>Item 7</li><li>Item 8</li><li>Item 9</li></ul></div>"`
     );
   });
 });
