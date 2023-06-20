0334f04375fee69a6932681568297796acf34529
Yegor G <yegorich_555@mail.ru>
2023-06-20 14:25:20 +0300

    Demo. Fix focus style for tabs

--- demo/src/elements/tabs.tsx
+++ demo/src/elements/tabs.tsx
@@ -11,7 +11,6 @@ interface Props {
 }
 
 // todo rewrite with <wup-tabs />: https://www.w3.org/WAI/ARIA/apg/patterns/tabs/examples/tabs-automatic/
-
 export default function Tabs(props: Props): JSX.Element {
   const [current, setCurrent] = useState(0);
   const onClick = useCallback((e: React.MouseEvent) => {
