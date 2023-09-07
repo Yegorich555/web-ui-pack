043fc28f844dba97bffc979eb8f7de6912455959
Yegorich555 <yegorich_555@mail.ru>
2023-09-07 08:44:37 +0300

    Html. Sort by tagNames - no changes

---
+++ types.html.update.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable global-require */
+const fs = require("fs");
+const htmlTypes = require("./types.html.json");
+
+try {
+  const tags = htmlTypes.tags.sort((a, b) => a.name.localeCompare(b.name));
+  // tags.forEach((tag) => {
+  //   console.warn(`Proccessed <${tag.name}>`);
+  //   const exclude = new Set(["disabled", "readonly"]);
+  //   tag.attributes.forEach((a) => {
+  //     a.name = a.name.toLowerCase();
+  //     if (!exclude.has(a.name) && !a.name.startsWith("w-")) {
+  //       a.name = `w-${a.name}`;
+  //     }
+  //     console.warn(`   [${a.name}]`);
+  //   });
+  // });
+  fs.writeFileSync("./types.html.json", JSON.stringify(htmlTypes, null, "\t"));
+} catch (err) {
+  console.error(err);
+}
+
+/** Run this script to update types.html.json. cmd> node ./types.html.update.js */
