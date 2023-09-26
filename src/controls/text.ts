3de1d3b8086c86f962714ca22a2fd34e26ec95ef
Yegor G <yegorich_555@mail.ru>
2023-09-26 10:38:53 +0300

    Add todos

--- src/controls/text.ts
+++ src/controls/text.ts
@@ -763,3 +763,5 @@ export default class WUPTextControl<
 customElements.define(tagName, WUPTextControl);
 // todo example how to create bult-in dropdown before the main input (like phone-number with ability to select countryCode)
 // gotInput > setMask > parseValue >... setValue ....> toString > setInput > setMask
+
+// todo show btnClear only when hover OR focus
--- src/helpers/dateCopyTime.ts
+++ src/helpers/dateCopyTime.ts
@@ -1,6 +1,5 @@
 /** Copy hh:mm:ss.fff part from B to A
- * @returns the same date pointed at first argument
- */
+ * @returns the same date pointed at first argument */
 export default function dateCopyTime(to: Date, from: Date, utc: boolean): Date {
   const u = utc ? "UTC" : "";
   to[`set${u}Hours`](
