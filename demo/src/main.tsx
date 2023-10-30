c8ba38615406c8f44ff448316cc61f1c2c6709f6
Yegorich555 <yegorich_555@mail.ru>
2023-10-30 14:45:29 +0200

    Demo. Fix params in router on github.io doesn't work

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -101,8 +101,9 @@ export default function AppContainer() {
     const prevPath = window.localStorage.getItem("path");
     if (prevPath) {
       window.localStorage.removeItem("path");
-      const r = routes.find((v) => v.path === prevPath);
-      r && navigate(r.url as string); // window.location.replace(r.url as string);
+      const p = prevPath.split("?")[0];
+      const r = routes.find((v) => v.path === p);
+      r && navigate(prevPath); // window.location.replace(r.url as string);
     }
   }, []);
 
